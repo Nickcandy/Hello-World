c06ff048f34ead0ca730ab61ef9eaafd0a7818c3
your name <609860367@qq.com>
2017-11-02 17:40:04 +0800

    edit

--- Helloworld.java
+++ Helloworld.java
@@ -2,5 +2,6 @@
 public class Helloworld {
 	public static void main(String[] args){
 		System.out.println("Hi, I'm nick.\nI'm a noob here.\nJust to practice.");
-	}
+	System.out.println("Hello World !!!);	
+}
 }
